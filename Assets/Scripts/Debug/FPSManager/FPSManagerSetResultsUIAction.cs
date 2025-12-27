6d0fefa3a4389a0cf29a5305c102be59ee281c54
MrMacRight <83794957+mrmacright@users.noreply.github.com>
2025-12-27 14:25:36 +1000

    Voice-over added and fixes for benchmark data

--- Assets/Scripts/Debug/FPSManager/FPSManagerSetResultsUIAction.cs
+++ Assets/Scripts/Debug/FPSManager/FPSManagerSetResultsUIAction.cs
@@ -24,10 +24,9 @@ public class FPSManagerSetResultsUIAction : ActionBase
         SpaceshipOptions o = GameOption.Get<SpaceshipOptions>();
         float p = o.screenPercentage / 100f;
         float mPix = (go.width * p * go.height * p) / 1000000;
-        string sp = o.screenPercentage == 100 ? $"Native" : $"{o.screenPercentage}% SP ({o.upsamplingMethod.ToString()})";
-
+        string sp = o.screenPercentage == 100 ? string.Empty : $"{o.screenPercentage}% SP ({o.upsamplingMethod})";
         FPSManager fpsm = Manager.Get<FPSManager>(); 
-        QualityText.text = $"{go.width}x{go.height}@{go.refreshRate}Hz ({go.fullScreenMode}) {sp} ({mPix.ToString("F2", System.Globalization.CultureInfo.InvariantCulture)} MPix) - {QualitySettings.names[QualitySettings.GetQualityLevel()]} Quality";
+        QualityText.text = $"{go.width}x{go.height} ({go.fullScreenMode}) {sp} - {QualitySettings.names[QualitySettings.GetQualityLevel()]} Quality";
         OverallFPSText.text = (1000 / fpsm.results.avgMs).ToString("F1", System.Globalization.CultureInfo.InvariantCulture);
         OverallMSText.text = fpsm.results.avgMs.ToString("F2", System.Globalization.CultureInfo.InvariantCulture);
         OverallMSPerMPixText.text = (fpsm.results.avgMs / mPix).ToString("F2", System.Globalization.CultureInfo.InvariantCulture);
@@ -35,8 +34,8 @@ public class FPSManagerSetResultsUIAction : ActionBase
         WorstMSText.text = fpsm.results.maxMs.ToString("F2", System.Globalization.CultureInfo.InvariantCulture);
         BestFPSText.text = (1000 / fpsm.results.minMs).ToString("F1", System.Globalization.CultureInfo.InvariantCulture);
         BestMSText.text = fpsm.results.minMs.ToString("F2", System.Globalization.CultureInfo.InvariantCulture);
-        CPUInfo.text = $"{SystemInfo.processorType} ({SystemInfo.processorCount} threads) @ { (SystemInfo.processorFrequency/1000f).ToString("F2", System.Globalization.CultureInfo.InvariantCulture)} GHz. ";
+        CPUInfo.gameObject.SetActive(false);
         RAMInfo.text = $"System Memory : { SystemInfo.systemMemorySize / 1000}GB";
-        GPUInfo.text = $"{ SystemInfo.graphicsDeviceName} ({ SystemInfo.graphicsDeviceType}) { SystemInfo.graphicsMemorySize / 1000}GB VRAM";
+        GPUInfo.text = GPUInfo.text = SystemInfo.graphicsDeviceName;
     }
 }
